b2aaaaf1731868f12e4ee25a8162e5392bc7106e
ivanstoimenovdb <ivanstoimenovdb@gmail.com>
2018-10-19 16:56:53 +0300

    Whole project is descripted
    The finaly descripted project.

--- facedetect.cpp
+++ facedetect.cpp
@@ -78,13 +78,22 @@ int main( int argc, const char** argv )
 	/*******************************************************************************
 	*                                Proccessing                                   *
 	*******************************************************************************/
-	// Checking of loading files.
+
+	/*******************************************************************************
+	*                              Loading parser                                  *
+	*******************************************************************************/
+	// Checking of loading parser description files.
     if (parser.has("help"))
     {
-		// if files not load it will point to help
+		// if parser files not load it will point to help
         help();
         return 0;
     }
+
+	/*******************************************************************************
+	*                           Setting name of detections                         *
+	*******************************************************************************/
+
 	// set Facedetection name 
     cascadeName = parser.get<string>("cascade");
 	
@@ -92,13 +101,21 @@ int main( int argc, const char** argv )
     nestedCascadeName = parser.get<string>("nested-cascade");
     scale = parser.get<double>("scale");
 
-	// Cheching scale is it too small
+	/*******************************************************************************
+	*                                Checking scale                                *
+	*******************************************************************************/
+
+	// Checking scale is it too small
     if (scale < 1)
         scale = 1;
     tryflip = parser.has("try-flip");
 
 	// Input the video file/device.
     inputName = parser.get<string>("@filename");
+
+	/*******************************************************************************
+	*                      Checking parser and description files                   *
+	*******************************************************************************/
 
 	// Cheching of is it loaded parser. 
     if (!parser.check())
@@ -111,46 +128,76 @@ int main( int argc, const char** argv )
 	// Checking of is it load description file of inner elements of face.
     if ( !nestedCascade.load( nestedCascadeName ) )
         cerr << "WARNING: Could not load classifier cascade for nested objects" << endl;
+
 	// Checking of is it load description file of face.
     if( !cascade.load( cascadeName ) )
     {
         cerr << "ERROR: Could not load classifier cascade" << endl;
+
 		// it return load is it doesnt load both description.
         help();
         return -1;
     }
+
+	/*******************************************************************************
+	*                          Checking of input files							   *
+	*							  and input devices                                *
+	*******************************************************************************/
+	
+	// Checking if input file which is processed of video capture is empty
     if( inputName.empty() || (isdigit(inputName[0]) && inputName.size() == 1) )
     {
         int camera = inputName.empty() ? 0 : inputName[0] - '0';
+
+		//  Checking is device is open
         if(!capture.open(camera))
+
+			// if it is not open connection with camera return this cout
             cout << "Capture from camera #" <<  camera << " didn't work" << endl;
     }
+	     // Checks for input size
     else if( inputName.size() )
     {
+		// Read input file of video file.
         image = imread( inputName, 1 );
         if( image.empty() )
         {
+			// if there is not input of video file
             if(!capture.open( inputName ))
+
+				// this is couting
                 cout << "Could not read " << inputName << endl;
         }
     }
     else
     {
+		// if there is video file it reads from folder E:\opencv\opencv-3.4.3\opencv-3.4.3\samples\data\lena.jpg
+		// But you should put your video file in this directory
         image = imread( "../data/lena.jpg", 1 );
+
+		// if there is not video file this is messaging
         if(image.empty()) cout << "Couldn't read ../data/lena.jpg" << endl;
     }
 
+	// Checks the object capture is open
     if( capture.isOpened() )
     {
         cout << "Video capturing has been started ..." << endl;
 
+		// it grabs all frames which are comming from device in infinity loop
         for(;;)
         {
             capture >> frame;
             if( frame.empty() )
                 break;
 
+			// Clone some figure (in our case is circle)
             Mat frame1 = frame.clone();
+			
+			/*******************************************************************************
+			*                              Detect and draw objects                         *
+			*******************************************************************************/
+			// Method whos draws and detects objects
             detectAndDraw( frame1, cascade, nestedCascade, scale, tryflip );
 
             char c = (char)waitKey(10);
@@ -160,19 +207,30 @@ int main( int argc, const char** argv )
     }
     else
     {
+		// This is detection of faces in video file  
         cout << "Detecting face(s) in " << inputName << endl;
+
+		// Checks is there image file
         if( !image.empty() )
         {
+			// Detect and draw frame in image (image file).
             detectAndDraw( image, cascade, nestedCascade, scale, tryflip );
             waitKey(0);
         }
+		 // checking if there is input video file.
         else if( !inputName.empty() )
         {
+			// read text file with list of image files. Proccesing is line by line.
+
             /* assume it is a text file containing the
             list of the image filenames to be processed - one per line */
             FILE* f = fopen( inputName.c_str(), "rt" );
             if( f )
             {
+				/*******************************************************************************
+				*              Reading row by row whole txt file with list of images           *
+				*******************************************************************************/
+
                 char buf[1000+1];
                 while( fgets( buf, 1000, f ) )
                 {
@@ -184,6 +242,7 @@ int main( int argc, const char** argv )
                     image = imread( buf, 1 );
                     if( !image.empty() )
                     {
+						// Detect and draw the object in all list of images.
                         detectAndDraw( image, cascade, nestedCascade, scale, tryflip );
                         char c = (char)waitKey(0);
                         if( c == 27 || c == 'q' || c == 'Q' )
@@ -191,9 +250,11 @@ int main( int argc, const char** argv )
                     }
                     else
                     {
+						// The image couldn't be readen
                         cerr << "Aw snap, couldn't read image " << buf << endl;
                     }
                 }
+				// Close file stream.
                 fclose(f);
             }
         }
@@ -202,12 +263,20 @@ int main( int argc, const char** argv )
     return 0;
 }
 
+/*******************************************************************************
+*                          Detect and draw method itself                       *
+*******************************************************************************/
+
 void detectAndDraw( Mat& img, CascadeClassifier& cascade,
                     CascadeClassifier& nestedCascade,
                     double scale, bool tryflip )
 {
     double t = 0;
+
+	// Variables that processing the face detect and inner face elements.
     vector<Rect> faces, faces2;
+
+	// Getting some color histogram
     const static Scalar colors[] =
     {
         Scalar(255,0,0),
@@ -219,6 +288,8 @@ void detectAndDraw( Mat& img, CascadeClassifier& cascade,
         Scalar(0,0,255),
         Scalar(255,0,255)
     };
+
+	// This variables get size of images and color of images.
     Mat gray, smallImg;
 
     cvtColor( img, gray, COLOR_BGR2GRAY );
@@ -242,21 +313,43 @@ void detectAndDraw( Mat& img, CascadeClassifier& cascade,
                                  //|CASCADE_DO_ROUGH_SEARCH
                                  |CASCADE_SCALE_IMAGE,
                                  Size(30, 30) );
+
+		// processing of face and inner elements of face.
         for( vector<Rect>::const_iterator r = faces2.begin(); r != faces2.end(); ++r )
         {
+			// Get size of face and elements of face.
             faces.push_back(Rect(smallImg.cols - r->x - r->width, r->y, r->width, r->height));
         }
     }
     t = (double)getTickCount() - t;
+	
+	// set frequency of detection and drawing. 
     printf( "detection time = %g ms\n", t*1000/getTickFrequency());
     for ( size_t i = 0; i < faces.size(); i++ )
     {
+		/*******************************************************************************
+		*                                 Drawing of frame.                            *
+		*******************************************************************************/
+
+		// Get face of previous infinity loop
         Rect r = faces[i];
+
+		// set scale of image to small image.
         Mat smallImgROI;
+
+		// Handle nested objects (eyes)
         vector<Rect> nestedObjects;
+
+		// this is center of an eyes
         Point center;
+
+		// this is blue color of frame.
         Scalar color = colors[i%8];
+
+		// variable which is handle radius.
         int radius;
+
+		// Drawing frame of the face and eyes by radio
 
         double aspect_ratio = (double)r.width/r.height;
         if( 0.75 < aspect_ratio && aspect_ratio < 1.3 )
@@ -289,5 +382,6 @@ void detectAndDraw( Mat& img, CascadeClassifier& cascade,
             circle( img, center, radius, color, 3, 8, 0 );
         }
     }
+	// Showing the whole frame whith detecte face and eyes.
     imshow( "result", img );
 }
